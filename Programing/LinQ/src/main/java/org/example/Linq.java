90dbb3f9287b1e13eb10a5dd016d5fa28422ca05
huytemp01 <huytemp01@gmail.com>
2024-09-12 19:07:14 +0700

    Implement orderBy(), orderByDescending(). + add testcase

--- Programing/LinQ/src/main/java/org/example/Linq.java
+++ Programing/LinQ/src/main/java/org/example/Linq.java
@@ -1,8 +1,12 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 
 public class Linq<T> {
@@ -11,8 +15,6 @@ public class Linq<T> {
     public static Linq of(List<? extends Object> elements){
         return new Linq(elements);
     }
-
-
 
     private Linq(List<T> numbers){
         for(T n:numbers){
@@ -23,5 +25,24 @@ public class Linq<T> {
 
     public List<T> Where(Predicate<T> p) {
         return elements.stream().filter(p).collect(Collectors.toList());
+    }
+
+    public List<T> orderBy(Comparator<T> comparator) {
+        return elements.stream().sorted(comparator).collect(Collectors.toList());
+    }
+
+    public List<T> orderBy(ToIntFunction<? super T> keyExtractor) {
+        Comparator<T> comparator = (c1, c2) -> Integer.compare(keyExtractor.applyAsInt(c1), keyExtractor.applyAsInt(c2));
+        return elements.stream().sorted(comparator).collect(Collectors.toList());
+    }
+
+    public <U extends Comparable<? super U>> List<T> orderBy(Function<? super T, ? extends U> keyExtractor) {
+        Comparator<T> comparator = (c1, c2) -> keyExtractor.apply(c1).compareTo(keyExtractor.apply(c2));
+        return elements.stream().sorted(comparator).collect(Collectors.toList());
+    }
+
+    public <U extends Comparable<? super U>> List<T> orderByDescending(Function<? super T, ? extends U> keyExtractor) {
+        Comparator<T> comparator = (c1, c2) -> keyExtractor.apply(c1).compareTo(keyExtractor.apply(c2));
+        return elements.stream().sorted(comparator.reversed()).collect(Collectors.toList());
     }
 }
--- Programing/LinQ/src/test/java/LinqTest.java
+++ Programing/LinQ/src/test/java/LinqTest.java
@@ -5,12 +5,16 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 
 public class LinqTest {
 
     @Test
-    public void test_where_with_list_int(){
+    public void test_where_should_return_elements(){
         //Arrange
         List<Integer> numbers = Arrays.asList(4,2,1,5,2,9);
         Linq<Integer> linq = Linq.of(numbers);
@@ -23,13 +27,13 @@ public class LinqTest {
     }
 
     @Test
-    public void test_where_with_list_Student(){
+    public void test_where_should_return_student_with_id_2(){
         //Arrange
         List<Student> students = new ArrayList<>();
         students.add(new Student(1,"Vu", "Duc Huy"));
         students.add(new Student(2,"Nguyen", "Minh Duc"));
         students.add(new Student(3,"Tran", "Thanh Man"));
-        students.add( new Student(4,"Le", "Thuan Thien"));
+        students.add(new Student(4,"Le", "Thuan Thien"));
         students.add(new Student(5,"Pham", "Quoc Khanh"));
         Linq<Student> linq = Linq.of(students);
         //Act
@@ -38,6 +42,72 @@ public class LinqTest {
         int expected = 1;
         Assert.assertEquals(expected,result.size());
         Assert.assertEquals(students.get(1), result.get(0));
-
     }
+
+    @Test
+    public void test_OrderBY_should_return_in_ascending_id_order(){
+        //Arrange
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(3,"Tran", "Thanh Man"));
+        students.add(new Student(5,"Pham", "Quoc Khanh"));
+        students.add(new Student(2,"Nguyen", "Minh Duc"));
+        students.add(new Student(4,"Le", "Thuan Thien"));
+        students.add(new Student(1,"Vu", "Duc Huy"));
+        Linq<Student> linq = Linq.of(students);
+        //Act
+        List<Student> result = linq.orderBy(Student::getId);
+        //Assert
+        Assert.assertEquals(result.get(0).getId(),1);
+        Assert.assertEquals(result.get(result.size()-1).getId(), 5);
+    }
+
+    @Test
+    public void test_OrderBY_should_return_in_descending_id_order(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(3,"Tran", "Thanh Man"));
+        students.add(new Student(5,"Pham", "Quoc Khanh"));
+        students.add(new Student(2,"Nguyen", "Minh Duc"));
+        students.add(new Student(4,"Le", "Thuan Thien"));
+        students.add(new Student(1,"Vu", "Duc Huy"));
+        Linq<Student> linq = Linq.of(students);
+        //Act
+        List<Student> result = linq.orderByDescending(Student::getId);
+        //Assert
+        Assert.assertEquals(result.get(0).getId(),5);
+        Assert.assertEquals(result.get(result.size()-1).getId(), 1);
+    }
+
+    @Test
+    public void test_OrderBY_should_return_in_ascending_firstname_order(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(3,"Tran", "Thanh Man"));
+        students.add(new Student(5,"Pham", "Quoc Khanh"));
+        students.add(new Student(2,"Nguyen", "Minh Duc"));
+        students.add(new Student(4,"Le", "Thuan Thien"));
+        students.add(new Student(1,"Vu", "Duc Huy"));
+        Linq<Student> linq = Linq.of(students);
+        //Act
+        List<Student> result = linq.orderBy(Student::getFirstName);
+        //Assert
+        Assert.assertEquals("Le",result.get(0).getFirstName());
+        Assert.assertEquals("Vu",result.get(result.size()-1).getFirstName() );
+    }
+
+    @Test
+    public void test_OrderBY_should_return_in_descending_firstname_order(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(3,"Tran", "Thanh Man"));
+        students.add(new Student(5,"Pham", "Quoc Khanh"));
+        students.add(new Student(2,"Nguyen", "Minh Duc"));
+        students.add(new Student(4,"Le", "Thuan Thien"));
+        students.add(new Student(1,"Vu", "Duc Huy"));
+        Linq<Student> linq = Linq.of(students);
+        //Act
+        List<Student> result = linq.orderByDescending(Student::getFirstName);
+        //Assert
+        Assert.assertEquals("Vu",result.get(0).getFirstName());
+        Assert.assertEquals("Le",result.get(result.size()-1).getFirstName() );
+    }
+
+
 }
